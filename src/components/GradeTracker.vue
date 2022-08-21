3c60768ea5733c8bda9c8ae7a7de057c2add2a79
King <kinglau66@hotmail.com>
2022-08-22 04:34:12 +0800

    fix: css as per figma

--- src/components/GradeTracker.vue
+++ src/components/GradeTracker.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="grade_cont">
-    <p class="flex flex-row">
+    <p class="flex flex-col">
       <small id="student_label">{{ grade?.name }}</small>
       <span
         id="student_id"
@@ -10,7 +10,6 @@
       </span>
       <span
         id="courses"
-        class="text-right"
       >
         {{ grade?.course_ids?.join(', ') }}
       </span>
@@ -51,4 +50,82 @@ onMounted(() => {
 </script>
 
 <style scoped>
+#grade_cont {
+  height: 210px;
+  width: 230px;
+  left: 957px;
+  top: 370px;
+  border-radius: 0px;
+
+  box-sizing: border-box;
+  background: #FFFFFF;
+  box-shadow: 0px 1px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+
+  & #student_label {
+
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+
+    color: #000000;
+  }
+
+  & #student_id {
+
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 48px;
+    line-height: 58px;
+
+    color: #000000;
+    margin: 0 0 0 0;
+  }
+
+  & #courses {
+
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 15px;
+
+    /* or 125% */
+
+    color: #000000;
+  }
+
+  & #gpa {
+
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 48px;
+    line-height: 58px;
+    display: flex;
+    align-items: flex-end;
+    text-align: right;
+
+    color: #000000;
+  }
+
+  & #gpa_label {
+
+    font-family: 'Lato';
+    font-style: normal;
+    font-weight: 500;
+    font-size: 12px;
+    line-height: 14px;
+    display: flex;
+    align-items: flex-end;
+    text-align: right;
+
+    color: #000000;
+  }
+}
 </style>
