435c27465f980e94f9ed92d612ff58cff7347fec
King <kinglau66@hotmail.com>
2022-08-22 02:55:29 +0800

    init: initialize git folder

---
+++ src/components/GradeTracker.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    (TODO)
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'GradeTracker',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
