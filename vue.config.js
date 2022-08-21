18d2dfb00e3c0fa9955aa6e7ca04d3345f44b76c
King <kinglau66@hotmail.com>
2022-08-22 03:48:52 +0800

    feat: proxy

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api/grades': {
+        target: 'https://quanmgx57hjiqicdxgo2vzebqq0tghim.lambda-url.ap-southeast-1.on.aws',
+        changeOrigin: true,
+      },
+      '/api/timeout': {
+        target: 'localhost:3000/timeouttest',
+        changeOrigin: true,
+      },
+    } 
+  }
 })
