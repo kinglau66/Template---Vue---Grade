5041e7a38ccaad1f7de53395d1e0de79b2aea153
King <kinglau66@hotmail.com>
2022-08-22 03:31:48 +0800

    feat: adding style for h1 title

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Students</h1>
+  <h1 class="student mt-5">Students</h1>
   <GradeTracker />
 </template>
 
@@ -10,8 +10,21 @@ GradeTracker
 
 <style>
 #app {
+  display: flex;
+  flex-direction: column;
   font-family: 'Lato', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
+}
+
+.student {
+  top: 3 rem;
+  font-family: 'Lato';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 36px;
+  line-height: 43px;
+  text-align: center;
+  color: #000000;
 }
 </style>
