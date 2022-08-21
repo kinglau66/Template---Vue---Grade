34d38d0ddef146a8a9bb77b5d125d6f52952d791
King <kinglau66@hotmail.com>
2022-08-22 03:52:02 +0800

    feat: added wretch api call

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,62 @@
 </template>
 
 <script setup lang="ts">
+import { onMounted, Ref, ref } from 'vue';
 import GradeTracker from './components/GradeTracker.vue';
+import Grade from './model/grade.model'
+import wretch from 'wretch';
+import { retry } from "wretch/middlewares/retry"
+import AbortAddon from "wretch/addons/abort"
+
 GradeTracker
+const API_URL = `api/grades`;
+let grades: any[] = []
+let studentmap: Map<String, Grade> = new Map();
+let students: Ref<Grade[]> = ref([])
+let grade: Grade = {}
+let isError: Ref<boolean> = ref(false)
+
+const init = () => {
+  grades = [];
+  studentmap = new Map();
+  students.value = [];
+  grade = {};
+  isError.value = false;
+}
+
+const setup = async () => {
+  const url = `${API_URL}`;
+  let fetchRes: any[] = await wretch(url)
+    .middlewares([retry({ retryOnNetworkError: true, maxAttempts: 2, until: (res)=> res?.clone().json().then(body =>body.length > 0) ?? false})])
+    .addon(AbortAddon())
+    .get()
+    .setTimeout(3000)
+    .onAbort(() => isError.value = true)
+    .internalError(() => isError.value = true)
+    .error(502, () => isError.value = true)
+    .json();
+  grades = fetchRes
+  // reducer to change map for proecssing purpose
+  const reducer = (prevMap: Map<String, any>, currItem: any) => {
+    let selectStudent = { ...prevMap.get(currItem.student_id) };
+    if (selectStudent?.student_id) {
+      selectStudent.accMarks += currItem.course_grade
+      selectStudent.course_ids = [...selectStudent.course_ids, currItem.course_id]
+    } else {
+      selectStudent = {};
+      selectStudent = currItem;
+      selectStudent.accMarks = currItem.course_grade;
+      selectStudent.course_ids = [currItem.course_id]
+    }
+    selectStudent.name = 'Student'
+    prevMap.set(currItem.student_id, selectStudent)
+    return prevMap;
+  }
+  studentmap = grades.reduce(reducer, new Map());
+  students.value = [...studentmap.values()]
+  console.log(studentmap)
+}
+onMounted(setup);
 </script>
 
 <style>
